10ba8cfa7e16bf91b91d6b426e9510471d86071e
lah <lah@webull.com>
2020-11-13 12:01:00 +0800

    移除时间和http包

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,62 @@
-const path = require('path');  
+const path = require('path');
 const entryPath = path.resolve(__dirname, `./lib/index.js`);
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = () => ({ 
+module.exports = () => ({
     entry: {
-        index : [entryPath],
+        index: [entryPath],
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].js',
-        library: "errorTool", 
+        library: "errorTool",
         libraryTarget: "umd"
     },
     mode: 'production',
     devtool: 'nosources-source-map',
     module: {
         rules: [
-                {
-                    test: /\.(js)?$/,
-                    loader : 'babel-loader',
-                    include: [path.resolve(__dirname,'./src')]
-                },
-                {
-                    test: /\.(ts)?$/,
-                    loader : 'babel-loader',
-                    exclude: /node_modules/,
-                    include: [path.resolve(__dirname,'./src')],
-                    options: {
-                        cacheDirectory: true,
-                        babelrc: false,
-                        sourceMaps: true,
-                        presets: ['@babel/preset-typescript','@babel/env'],
-                        plugins : [
-                            '@babel/plugin-transform-runtime',
-                            '@babel/plugin-transform-typescript',
-                            '@babel/plugin-transform-modules-commonjs', 
-                        ]
-                    }
-                }, 
+            {
+                test: /\.(js)?$/,
+                loader: 'babel-loader',
+                include: [path.resolve(__dirname, './src')],
+                options: {
+                    cacheDirectory: true,
+                    babelrc: false,
+                    sourceMaps: true,
+                    presets: [["@babel/preset-env", {
+                        "targets": {
+                            "browsers": [ "ie >= 10", "chrome >= 58" ] 
+                        }
+                    }]],
+                    plugins: [
+                        '@babel/plugin-transform-runtime', 
+                        '@babel/plugin-transform-modules-commonjs',
+                    ]
+                }
+            },
+            {
+                test: /\.(ts)?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, './src')],
+                options: {
+                    cacheDirectory: true,
+                    babelrc: false,
+                    sourceMaps: true,
+                    presets: ['@babel/preset-typescript', ["@babel/preset-env", {
+                        "targets": {
+                            "browsers": [ "ie >= 8", "chrome >= 58" ] 
+                        }
+                    }]],
+                    plugins: [
+                        '@babel/plugin-transform-runtime',
+                        '@babel/plugin-transform-typescript',
+                        '@babel/plugin-transform-modules-commonjs',
+                    ]
+                }
+            },
         ]
     },
     resolve: {
@@ -46,14 +64,12 @@ module.exports = () => ({
         modules: [path.resolve('./src'), 'node_modules']
     },
     plugins: [
-        new CleanWebpackPlugin(), 
-    ], 
+        new CleanWebpackPlugin(),
+    ],
     optimization: {
         minimize: true,
         minimizer: [
-            new TerserPlugin({ // 使用压缩插件
-                include: /\.min\.js$/
-            })
+            new TerserPlugin({})
         ]
     }
 });
